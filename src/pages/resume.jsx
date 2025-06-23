baa0401ef9d3855bd6d9f78b0546d6e8bbf265d4
Deepak Kumar <deepakjct654321@outlook.com>
2025-06-23 13:41:48 +0530

    edit resume.jsx

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -10,6 +10,11 @@ function PDFViewer() {
                 title="PDF Viewer"
                 style={{ border: 'none' }}
             ></iframe>
+            <div className="text-center mt-3">
+                <a href={`${process.env.PUBLIC_URL}/Deepak_resume.pdf`} className="btn btn-primary" download>
+                    Download Resume
+                </a>
+        </div>
         </div>
     );
 }
